eb6c2db7b293ff49e7d4294863412a3bb3ecc533
Nathan Miller <nmiller93@gatech.edu>
2022-03-20 17:14:18 -0700

    Added states to US_powerplants data set. General cleanup and TODO's added to JS code.

--- visualization.js
+++ visualization.js
@@ -1,27 +1,51 @@
 // Define margin and dimensions for svg
 var margin = {top: 20, right: 10, bottom: 20, left: 10};
 var padding = 30;
-var outerWidth = 1600;
-var outerHeight = 800;
+var outerWidth = 1024;
+var outerHeight = 768;
 var width = outerWidth - padding;
 var height = outerHeight - padding;
 
-// Global variables
+// Variables to store loaded data
 var STATE_MAP;  // Store state map data from json
 var COUNTY_MAP;  // Store county map data from json
 var PLANT_DATA;  // Store power plant data from csv
-var CURRENT_VIEW = 'state';
+var STATE_DATA;  // Store power plant data by state
 
+var CURRENT_VIEW = 'state';  // Could be used to switch between showing state or county lines 
+const STARTING_SCALE = 0.65  // This changes how big the US map is at the start
+const format2decimals = d3.format(".2f")
 
-// Create svg
-var svg = d3.select("body").append("svg")
+// Create main choropleth map svg
+var svg = d3.select(".layer3").append("svg")
             .attr("width", outerWidth)
             .attr("height", outerHeight)
             .attr("id", "choropleth") 
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
             .attr("style", "outline: thin solid black;");
 
+// Create seperate SVG for the legend
+// Not sure we want to do this or if we even need a legend 
+var legend_svg = d3.select(".layer3").append("svg")
+                   .attr("width", 200)
+                   .attr("height", 200)
+                   .attr("id", "box1") 
+                   .attr("transform", "translate(" + (margin.left * 2) + "," + (margin.top - outerHeight + 200) + ")")
+                   .attr("style", "outline: thin solid black;");
+
+// Create another svg for a widget that will display stats about a state
+// This is not currently being used
+/*
+var stats_svg = d3.select(".layer3").append("svg")
+                  .attr("width", 400)
+                  .attr("height", 300)
+                  .attr("id", "box2") 
+                  .attr("transform", "translate(" + (margin.left * 2 - 200) + "," + (margin.top - outerHeight + 500) + ")")
+                  .attr("style", "outline: thin solid black;");
+*/
+
 // Define tooltip when hovering over a state
+// TODO: Add stats for the state based on selected fuel type
 var state_tooltip = d3.tip()
                       .attr('id', 'tooltip')
                       .html(function(d) { return String(d.properties.name); });
@@ -29,11 +53,12 @@ var state_tooltip = d3.tip()
 // Define tooltip when hovering over a power plant
 var plant_tooltip = d3.tip()
                       .attr('id', 'tooltip')
-                      .html(function(d) { return String(
+                      .html(function(d) { 
+                              return String(
                               "Name: " + d.name + "<br/>" + 
                               "Year Commissioned: " + d.commissioning_year + "<br/>" +
                               "Owner: " + d.owner + "<br/>" +
-                              "Yearly Production: " + d.generation_gwh_2019)
+                              "Yearly Production: " + format2decimals(d.generation_gwh_2019))
                       });
 
 // Define color scheme
@@ -52,7 +77,7 @@ var path = d3.geoPath()
              .projection(projection);
 
 
-// Promise to get map data
+// Promise to get county map data
 const promise_county_map = d3.json("gz_2010_us_050_00_20m.json", function(json) {
                               svg.selectAll("path")
                                  .data(json.features)
@@ -61,6 +86,7 @@ const promise_county_map = d3.json("gz_2010_us_050_00_20m.json", function(json) 
                                  .attr("d", path) 
                             });
 
+// Promise to get state map data
 const promise_state_map = d3.json("us-states.json", function(json) {
                             svg.selectAll("path")
                                .data(json.features)
@@ -69,7 +95,7 @@ const promise_state_map = d3.json("us-states.json", function(json) {
                                .attr("d", path) 
                             });
 
-// Promise to get plant data
+// Promise to get power plant data
 const promise_plant_data = d3.dsv(",", "dataset/us_powerplants.csv", function (d) { return {
                                 // Define how data will be formatted
                                 name: d.name,
@@ -105,14 +131,32 @@ const promise_plant_data = d3.dsv(",", "dataset/us_powerplants.csv", function (d
                                 estimated_generation_note_2014: d.estimated_generation_note_2014,
                                 estimated_generation_note_2015: d.estimated_generation_note_2015,
                                 estimated_generation_note_2016: d.estimated_generation_note_2016,
-                                estimated_generation_note_2017: d.estimated_generation_note_2017
+                                estimated_generation_note_2017: d.estimated_generation_note_2017,
+                                state: d.state
                             }});
+
+
+/**
+ * TODO: Write this function (might be able to accomplish this with D3.nest() instead)
+ * @param {object} plant_data - a list of plants for a given fuel type
+ * @return {object} - a list of all states and their generation for the given fuel type
+ */  
+function getPowerGenerationByState(plant_data) {
+
+};
+
 
 Promise.all([promise_state_map, promise_county_map, promise_plant_data]).then((values, error) => {
     // Read in data files
     STATE_MAP = values[0]
-    COUNTY_MAP = values[1]
+    COUNTY_MAP = values[1]  // County map is not currently used
     PLANT_DATA = values[2]
+ 
+    // TODO: Implement nest function to get stats for each state
+    //STATE_DATA = d3.nest()
+    //               .key(d => d.state).sortKeys((a, b) => d3.ascending(a, b))
+    //               .rollup(d => d.state)
+    //               .entries(PLANT_DATA);
     
     // Display the map
     ready(error, STATE_MAP, PLANT_DATA);
@@ -120,29 +164,25 @@ Promise.all([promise_state_map, promise_county_map, promise_plant_data]).then((v
 });
 
 
-
-// TODO: Write this function
-function getPowerGenerationByState(plant_data) {
-    // plant_data: a list of plants for a given fuel type
-    // return: a list of all states and their generation for the given fuel type 
-    
-
-};
-
-
-
-// this function should be called once the data from files have been read
-// state_map: topojson from state_map_countries.json
-// plantdata: data from plants-by-country.csv
-function ready(error, state_map, plantdata) {
+/**
+ * TODO: Rename this function and make its purpose more specific. It is a remnant of the HW assignment. 
+ * This function: 
+ * 1) Adds fuel types to the dropdown menu
+ * 2) Sets up an event listener for the dropdown menu
+ * 3) Calls createMapAndLegend()
+ * @param {object} error - null if no errors in retrieving data, otherwise the error description 
+ * @param {object} state_map - geoJSON coordinates of the US states
+ * @param {object} plant_data - US power plant data
+ * @return {undefined}
+ */
+function ready(error, state_map, plant_data) {
     if (error) {
         // TODO: manage errors
-        // throw error;
-        // console.log(error);
+        
     }
     
-    // Get plant primary fuel types from plantdata
-    plant_types = d3.map(plantdata, function(d){return d.primary_fuel}).keys()
+    // Get plant primary fuel types from plant_data
+    plant_types = d3.map(plant_data, function(d){return d.primary_fuel}).keys()
                    .sort(d3.ascending);
     
     // Add plant fuel types to dropdown menu
@@ -157,18 +197,22 @@ function ready(error, state_map, plantdata) {
     // Event listener for plant dropdown menu change
     d3.select("#plantDropdown")
       .on("change", function() {
-        createMapAndLegend(state_map, plantdata, this.value)
+        createMapAndLegend(state_map, plant_data, this.value)
     });
     
     // Create Choropleth with default option 
-	createMapAndLegend(state_map, plantdata, d3.select('#plantDropdown option:checked').text());
+	createMapAndLegend(state_map, plant_data, d3.select('#plantDropdown option:checked').text());
 }
 
 
-
-// This function will create a Choropleth and legend using the state_map and plantdata
-// and update the map and legend when a different plant is selected from the dropdown
-function createMapAndLegend(state_map, plantdata, selectedPlant){ 
+/**
+ * Create a Choropleth map and legend using the state_map, plant_data, and fuel type
+ * @param {object} state_map - geoJSON coordinates of the US states
+ * @param {object} plant_data - US power plant data
+ * @param {string} selected_fuel - the fuel source the user selected from the dropdown 
+ * @return {undefined}
+ */
+function createMapAndLegend(state_map, plant_data, selected_fuel){ 
     
     d3.select("#states").remove();
     d3.select("#power_plants").remove();
@@ -181,13 +225,14 @@ function createMapAndLegend(state_map, plantdata, selectedPlant){
     // Shade state based on energy production by selected fuel type
     
     // Filter plants to only include those of selected fuel type
-    filtered_plants = plantdata.filter(function(d) {
-        return d.primary_fuel === selectedPlant;
+    filtered_plants = plant_data.filter(function(d) {
+        return d.primary_fuel === selected_fuel;
     })
     
     
-    // TODO: Currently this selects only the generation from 2019 for each plant
-    //       Need to update this to calculate total generation for each state
+    // TODO: Currently this selects only the generation from 2019 for each plant.
+    //       Need to update this to calculate total generation for each state.
+    //       Probably also need to remove outlier values.
     plants_list = filtered_plants.map(function(d) {
        return d["generation_gwh_2019"]
     });
@@ -221,7 +266,7 @@ function createMapAndLegend(state_map, plantdata, selectedPlant){
 				.call(d3.zoom().on("zoom", zooming))  // Bind the zoom behavior
                 .call(zoom.transform, d3.zoomIdentity  // Apply the initial transform
 							.translate(width/2, height/2)
-							.scale(0.75));
+							.scale(STARTING_SCALE));
     
     // Define a group element to hold the states
     var states = map.append("g")
@@ -244,7 +289,10 @@ function createMapAndLegend(state_map, plantdata, selectedPlant){
 	var power_plants = map.append("g")
         .attr("id", "power_plants")
 	
-	// Plot power plants
+	// Plot power plants as circles
+	// TODO: Currently all plants for the selected fuel type are displayed.
+	//       This could be changed to show only top x number of plants
+	//       with radius depending on the size of the power plant.
 	power_plants.selectAll("circle")
 		        .data(filtered_plants)
 		        .enter()
@@ -264,9 +312,9 @@ function createMapAndLegend(state_map, plantdata, selectedPlant){
                 .on('mouseout', plant_tooltip.hide);          
     
     // Define a group element for the legend
-    var g = svg.append("g")
+    var g = legend_svg.append("g")
         .attr("id", "legend")
-        .attr("transform", "translate(" + 800 + ",20)");
+        .attr("transform", "translate(10,10)");
     
     // Define the legend
     var legend = d3.legendColor()
@@ -275,14 +323,13 @@ function createMapAndLegend(state_map, plantdata, selectedPlant){
         .scale(colorScale);
     
     // Call the legend
-    svg.select("#legend")
+    legend_svg.select("#legend")
         .call(legend);
                
 }
 
 
-
-//Define what to do when panning or zooming
+// Define what to do when panning or zooming
 var zooming = function(d) {
 
 	//Log out d3.event.transform, so you can see all the goodies inside
