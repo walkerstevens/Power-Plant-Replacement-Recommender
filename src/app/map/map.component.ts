1026536196dd65e487a936fc0f0f242c929dc61b
Nathan Miller <nmiller93@gatech.edu>
2022-04-16 10:37:57 -0700

    Made color based solely on primary_fuel

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,7 +2,7 @@ import { ApplicationRef, Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Map } from 'mapbox-gl';
 import { MainServiceService } from '../../app/main-service.service'
-import { select } from 'd3';
+import { select, scaleLinear } from 'd3';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -59,83 +59,13 @@ export class MapComponent implements OnInit {
     });
   }
   
-  //let colors = {};
-  
   createPowerPlantCircles() {
     this.powerPlantCircles = this.svg.selectAll("circle")
       .data(this.powerPlantsFiltered)
       .enter()
       .append("circle")
-      .attr("r", (d: any) => { if (d.capacity_mw < 2.6) 
-                               {
-                                 return 3.5; 
-                               }
-                               else
-                               {
-                                 return 3.5 + (8.5 * (d.capacity_mw / 6809)); 
-                               }
-                             })
-      .style("fill", (d: any) => { // Shades of red
-                                   if (d.primary_fuel == "Coal")
-                                   {
-                                     return "#CF352E"; 
-                                   }
-                                   else if (d.primary_fuel == "Petcoke")
-                                   {
-                                     return "#FF0038"; 
-                                   }
-                                   else if (d.primary_fuel == "Oil") 
-                                   {
-                                     return "#8A0707"; 
-                                   }
-                                   else if (d.primary_fuel == "Gas")
-                                   {
-                                     return "#FF0800"; 
-                                   }
-                                   else if (d.primary_fuel == "Cogeneration")
-                                   {
-                                     return "#E32636"; 
-                                   }
-                                   // Shades of blue
-                                   else if (d.primary_fuel == "Solar")
-                                   {
-                                     return "#00BFFF"; 
-                                   }
-                                   else if (d.primary_fuel == "Wind")
-                                   {
-                                     return "#0073CF"; 
-                                   }
-                                   else if (d.primary_fuel == "Geothermal")
-                                   {
-                                     return "#1CA9C9"; 
-                                   }
-                                   else if (d.primary_fuel == "Hydro")
-                                   {
-                                     return "#1B5583"; 
-                                   }     
-                                   else if (d.primary_fuel == "Waste")
-                                   {
-                                     return "#4682B4"; 
-                                   }
-                                   else if (d.primary_fuel == "Biomass")
-                                   {
-                                     return "#969696"; 
-                                   }
-                                   // Purple
-                                   else if (d.primary_fuel == "Nuclear")
-                                   {
-                                     return "#6a51a3"; 
-                                   }
-                                   // Shades of grey
-                                   else if (d.primary_fuel == "Storage")
-                                   {
-                                     return "#778899"; 
-                                   }
-                                   else
-                                   {
-                                     return "#C0C0C0"; 
-                                   }
-                                 })
+      .attr("r", (d: any) => { return dotRadius(d) })
+      .style("fill", (d: any) => { return dotColor(d) })
       .style("stroke", "#ffffff")
       .style("cursor", "pointer");
 
@@ -189,10 +119,7 @@ export class MapComponent implements OnInit {
     this._mainService.fuelFilter$.subscribe((fuelFilter) => {
       // Get all power plants that match the filter 
       this.powerPlantsFiltered = this.powerPlants.filter((powerPlant: any) => {
-        return fuelFilter.includes(powerPlant.primary_fuel) ||
-               fuelFilter.includes(powerPlant.other_fuel1) ||
-               fuelFilter.includes(powerPlant.other_fuel2) ||
-               fuelFilter.includes(powerPlant.other_fuel3)
+        return fuelFilter.includes(powerPlant.primary_fuel)
       });
 
       // Delete circles
@@ -202,3 +129,75 @@ export class MapComponent implements OnInit {
     });
   }
 }
+
+var dotRadius = function(d: any) { if (d.capacity_mw < 2.6) 
+                                   {
+                                     return 3.5; 
+                                   }
+                                     else
+                                   {
+                                     return 3.5 + (8.5 * (d.capacity_mw / 6809)); 
+                                   }
+                                 }
+                                 
+var dotColor = function(d: any) { // Shades of red
+                                  if (d.primary_fuel == "Coal")
+                                  {
+                                    return "#CF352E"; 
+                                  }
+                                  else if (d.primary_fuel == "Petcoke")
+                                  {
+                                    return "#FF0038"; 
+                                  }
+                                  else if (d.primary_fuel == "Oil") 
+                                  {
+                                    return "#8A0707"; 
+                                  }
+                                  else if (d.primary_fuel == "Gas")
+                                  {
+                                    return "#FF0800"; 
+                                  }
+                                  else if (d.primary_fuel == "Cogeneration")
+                                  {
+                                    return "#E32636"; 
+                                  }
+                                  // Shades of blue
+                                  else if (d.primary_fuel == "Solar")
+                                  {
+                                    return "#00BFFF"; 
+                                  }
+                                  else if (d.primary_fuel == "Wind")
+                                  {
+                                    return "#0073CF"; 
+                                  }
+                                  else if (d.primary_fuel == "Geothermal")
+                                  {
+                                   return "#1CA9C9"; 
+                                  }
+                                  else if (d.primary_fuel == "Hydro")
+                                  {
+                                    return "#1B5583"; 
+                                  }     
+                                  else if (d.primary_fuel == "Waste")
+                                  {
+                                    return "#4682B4"; 
+                                  }
+                                  else if (d.primary_fuel == "Biomass")
+                                  {
+                                    return "#969696"; 
+                                  }
+                                  // Purple
+                                  else if (d.primary_fuel == "Nuclear")
+                                  {
+                                    return "#6a51a3"; 
+                                  }
+                                  // Shades of grey
+                                  else if (d.primary_fuel == "Storage")
+                                  {
+                                    return "#778899"; 
+                                  }
+                                  else
+                                  {
+                                    return "#C0C0C0"; 
+                                  }};
+
