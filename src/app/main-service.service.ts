731542917b6a1e9e4d2dfb9143883ee11b009cf0
Nathan Miller <nmiller93@gatech.edu>
2022-04-16 11:34:11 -0700

    Edited Power Plant Info box

--- src/app/main-service.service.ts
+++ src/app/main-service.service.ts
@@ -26,40 +26,13 @@ export class MainServiceService {
       function (d) {
         return {
           // Define how data will be formatted
-          name: d["name"],
-          gppd_idnr: d["gppd_idnr"],
+          Name: d["name"],
           capacity_mw: +d["capacity_mw"]!,
           latitude: +d["latitude"]!,
           longitude: +d["longitude"]!,
           primary_fuel: d["primary_fuel"],
-          other_fuel1: d["other_fuel1"],
-          other_fuel2: d["other_fuel2"],
-          other_fuel3: d["other_fuel3"],
           commissioning_year: d["commissioning_year"],
-          owner: d["owner"],
-          source: d["source"],
-          url: d["url"],
-          geolocation_source: d["geolocation_source"],
-          wepp_id: d["wepp_id"],
-          year_of_capacity_data: +d["year_of_capacity_data"]!,
-          generation_gwh_2013: +d["generation_gwh_2013"]!,
-          generation_gwh_2014: +d["generation_gwh_2014"]!,
-          generation_gwh_2015: +d["generation_gwh_2015"]!,
-          generation_gwh_2016: +d["generation_gwh_2016"]!,
-          generation_gwh_2017: +d["generation_gwh_2017"]!,
-          generation_gwh_2018: +d["generation_gwh_2018"]!,
-          generation_gwh_2019: +d["generation_gwh_2019"]!,
-          generation_data_source: d["generation_data_source"],
-          estimated_generation_gwh_2013: +d["estimated_generation_gwh_2013"]!,
-          estimated_generation_gwh_2014: +d["estimated_generation_gwh_2014"]!,
-          estimated_generation_gwh_2015: +d["estimated_generation_gwh_2015"]!,
-          estimated_generation_gwh_2016: +d["estimated_generation_gwh_2016"]!,
-          estimated_generation_gwh_2017: +d["estimated_generation_gwh_2017"]!,
-          estimated_generation_note_2013: d["estimated_generation_note_2013"],
-          estimated_generation_note_2014: d["estimated_generation_note_2014"],
-          estimated_generation_note_2015: d["estimated_generation_note_2015"],
-          estimated_generation_note_2016: d["estimated_generation_note_2016"],
-          estimated_generation_note_2017: d["estimated_generation_note_2017"]
+          Owner: d["owner"],
       }
     }).then((data) => {
       // Emit to subscribers the data
